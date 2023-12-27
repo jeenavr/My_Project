a8b90b92208423e5c735773aee553d72b1462cf5
jeenavr <151428251+jeenavr@users.noreply.github.com>
2023-12-27 22:30:28 +0530

    TMS.sql

--- TMS.sql
+++ TMS.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE TaskManagementSystem;
 
 CREATE TABLE users(
-	user_id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id INT PRIMARY KEY,
 	user_name VARCHAR(20) NOT NULL UNIQUE,
     email VARCHAR(255),
     password VARCHAR(20) NOT NULL,
@@ -19,54 +19,50 @@ CREATE TABLE tasks (
 );
 
 #Create new user:
-INSERT INTO users(user_name,email,password,confirm_password)
-VALUES ('Anna','anna@gmail.com','anna123','anna123');
+INSERT INTO users(user_id,user_name,email,password,confirm_password)
+VALUES (101,'Anna','anna@gmail.com','anna123','anna123');
 
-INSERT INTO users(user_name,email,password,confirm_password)
-VALUES ('Smith','smith@gmail.com','smith123','smith123');
+INSERT INTO users(user_id,user_name,email,password,confirm_password)
+VALUES (102,'Smith','smith@gmail.com','smith123','smith123');
 
-INSERT INTO users(user_name,email,password,confirm_password)
-VALUES ('Jeena','jeena@gmail.com','jeena123','jeena123');
+INSERT INTO users(user_id,user_name,email,password,confirm_password)
+VALUES (103,'Jeena','jeena@gmail.com','jeena123','jeena123');
 
-INSERT INTO users(user_name,email,password,confirm_password)
-VALUES ('Aaron','aaron@gmail.com','aaron123','aaron123');
+INSERT INTO users(user_id,user_name,email,password,confirm_password)
+VALUES (104,'Aaron','aaron@gmail.com','aaron123','aaron123');
 
-INSERT INTO users(user_name,email,password,confirm_password)
-VALUES ('Johan','johan@gmail.com','johan123','johan123');
+INSERT INTO users(user_id,user_name,email,password,confirm_password)
+VALUES (105,'Johan','johan@gmail.com','johan123','johan123');
 
-INSERT INTO users(user_name,email,password,confirm_password)
-VALUES ('Aadya','aadya@gmail.com','aadya123','aadya123');
+INSERT INTO users(user_id,user_name,email,password,confirm_password)
+VALUES (106,'Aadya','aadya@gmail.com','aadya123','aadya123');
 
 SELECT * FROM users;
 
 #Create  New Tasks:
 INSERT INTO tasks (user_id, title, description, due_date,status)
-VALUES (1, 'Develop a Plan', 'Create a comprehensive project roadmap 
+VALUES (101, 'Develop a Plan', 'Create a comprehensive project roadmap 
 outlining goals, tasks, and timelines.', '2023-01-09','Incomplete');
 
 INSERT INTO tasks (user_id, title, description, due_date,status)
-VALUES (1, 'Assign Tasks', 'Distribute specific responsibilities and 
+VALUES (102, 'Assign Tasks', 'Distribute specific responsibilities and 
 assignments to team members or contributors.','2023-08-17','Incomplete');
 
 INSERT INTO tasks (user_id, title, description, due_date,status)
-VALUES (2, 'Obtain Hardware', 'Acquire the necessary physical equipment 
+VALUES (103, 'Obtain Hardware', 'Acquire the necessary physical equipment 
 and infrastructure required for the project.', '2023-09-25','Incomplete');
 
 INSERT INTO tasks (user_id, title, description, due_date,status)
-VALUES (3,'Programming', 'Write, implement, and test the code according 
+VALUES (104,'Programming', 'Write, implement, and test the code according 
 to the specified project requirements.', '2023-11-19','Incomplete');
 
 INSERT INTO tasks (user_id, title, description, due_date,status)
-VALUES (3,'Install Hardware', 'Physically set up and install the
+VALUES (105,'Install Hardware', 'Physically set up and install the
 hardware components in the designated environment.', '2023-03-11','Incomplete');
 
 INSERT INTO tasks (user_id, title, description, due_date,status)
-VALUES (4,'Program Test','Execute rigorous testing procedures on the 
+VALUES (106,'Program Test','Execute rigorous testing procedures on the 
 programmed software to identify and fix potential issues.', '2023-04-23','Incomplete');
-
-INSERT INTO tasks (user_id, title, description, due_date,status)
-VALUES (6,'Write User Manual','Create a clear and comprehensive guide explaining 
-how users can effectively utilize the system or software.','2023-12-19','Incomplete');
 
 #Display a List of Tasks:
 SELECT * FROM tasks;
@@ -77,11 +73,14 @@ SELECT user_id FROM users;
 #Mark Task as Completed:
 UPDATE tasks
 SET status = 'Completed'
-WHERE user_id = 3;
+WHERE user_id = 103;
+
+#Search Task:
+SELECT * FROM tasks WHERE user_id=103;
 
 #Delete a Task:
 DELETE FROM tasks
-WHERE task_id = 6;
+WHERE use_id = 101;
 
 
 
